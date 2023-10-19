fe4cd6525fe2a9103045e4f185e68dd34721d07b
unoffending <109032978+unoffending@users.noreply.github.com>
2023-10-20 00:07:15 +0600

    Update FibonacciSeries.java

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+       int a = 0;
+       int b = 1;
+       int c = 0;
+       
+       while(b <= lastFibonacci) {
+           System.out.println(a);
+           System.out.println(b); 
+           c = a + b;
+           a = b + c;
+           b = a + c;
+           if(c > lastFibonacci) { break; }
+           System.out.println(c);
+       } 
     }
 }
