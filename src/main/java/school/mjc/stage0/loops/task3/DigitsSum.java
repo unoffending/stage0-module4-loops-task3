ffd1d992a71d7b6d08e51d0cb2e742df7181aeeb
unoffending <109032978+unoffending@users.noreply.github.com>
2023-10-20 00:06:19 +0600

    Update DigitsSum.java

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-
+        String a = Integer.toString(t);
+        int l = a.length();
+        int result = 0;
+        int remainder;
+        for(int i = l; i > 0; i--) {
+            remainder = t % 10;
+            result = result + remainder;
+            t = t / 10;
+        }
+        System.out.println(result);
     }
 }
