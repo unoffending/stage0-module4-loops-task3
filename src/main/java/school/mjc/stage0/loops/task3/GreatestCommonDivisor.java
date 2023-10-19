546c35e9f0f2cf55f39f07d9e133157d9ab415e7
unoffending <109032978+unoffending@users.noreply.github.com>
2023-10-20 00:07:52 +0600

    Update GreatestCommonDivisor.java

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+            for(int i = 9; i > 0; i--) {
+            if(first % i == 0 && second % i == 0) {
+                System.out.println(i);
+                break;
+            } 
+         }
     }
 }
