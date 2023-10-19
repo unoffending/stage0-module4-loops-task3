e4f6fc702abf638189d7cc41826342f2d79d9fce
unoffending <109032978+unoffending@users.noreply.github.com>
2023-10-20 00:04:24 +0600

    Update AlphabetPrinter.java

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+        for(char i = 65; i <= 90; i++) {
+            System.out.println(i);
+        }
     }
 }
