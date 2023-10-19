8069b3c84f4bf71391eaa77572b781fe6f02ba90
unoffending <109032978+unoffending@users.noreply.github.com>
2023-10-20 00:09:00 +0600

    Update RangeSum.java

--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -2,5 +2,6 @@ package school.mjc.stage0.loops.task3;
 
 public class RangeSum {
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
+        System.out.println(firstBoarder + secondBoarder);
     }
 }
