b4b181caefac49846ad197b1e1d40088ff1dff11
unoffending <109032978+unoffending@users.noreply.github.com>
2023-10-20 00:08:33 +0600

    Update NinesSum.java

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+         String a = Integer.toString(lengthOfLastNumber);
+         int l = a.length();
+         int nines = 9;
+         int result2 = 0;
+         int s;
+         for(int i = l; i > 0; i--) {
+             s = nines * l;
+             l--;
+             nines *= 10;
+             result2 += s;
+         } 
+         System.out.println(result2);
     }
 }
